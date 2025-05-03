8fa6ef4d1fdcc2c7c62cdda897d98a2270809dd2
Lucas Valdez <lnvaldez24@gmail.com>
2025-05-03 12:13:15 -0300

    backend-02-template

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+tareas = [
+    {
+        "titulo": "Comprar lechuga",
+        "fecha": "2025-05-05",
+        "descripcion": "Ir al super y comprar lechuga",
+        "email": "lucas@ejemplo.com",
+    },
+    {
+        "titulo": "Estudiar Flask",
+        "fecha": "2025-05-06",
+        "descripcion": "Nde tavyyyyy",
+        "email": "lucas@ejemplo.com",
+    },
+]
+
 @app.route("/")
 def home():
-    return "Hola mundo desde Flask!"
+    return render_template("index.html", tareas=tareas)
 
 if __name__ == "__main__":
     app.run(debug=True)
