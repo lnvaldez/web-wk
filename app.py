9ba389ca6ac7ea9cc103e037a9a52b1acdc40387
Lucas Valdez <lnvaldez24@gmail.com>
2025-05-03 12:23:09 -0300

    backend-03-form-handling

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-tareas = [
-    {
-        "titulo": "Comprar lechuga",
-        "fecha": "2025-05-05",
-        "descripcion": "Ir al super y comprar lechuga",
-        "email": "lucas@ejemplo.com",
-    },
-    {
-        "titulo": "Estudiar Flask",
-        "fecha": "2025-05-06",
-        "descripcion": "Nde tavyyyyy",
-        "email": "lucas@ejemplo.com",
-    },
-]
+tareas = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+
+    if request.method == "POST":
+        nueva_tarea = {
+            "titulo": request.form["titulo"],
+            "fecha": request.form["fecha"],
+            "descripcion": request.form["descripcion"],
+            "email": request.form["email"],
+        }
+        tareas.append(nueva_tarea)
+        return redirect("/")
+
     return render_template("index.html", tareas=tareas)
 
 if __name__ == "__main__":
